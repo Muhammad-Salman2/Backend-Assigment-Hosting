32a07adde832745928bb3029a54e272a69975978
salman <salman7official7@gmail.com>
2025-01-02 01:05:44 +0500

    code push

--- README.md
+++
@@ -1,1 +0,0 @@
-# backend-Assigment-hosting
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { products } from "./ProductData.js";
+import cors from "cors"
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+app.use(cors())
+
+app.get("/", (req, response) => {
+//   console.log(response.send(`hello world ${req.params.id}`));
+
+  // response.send("hello World");
+  console.log("hello")
+ 
+});
+
+app.get("/product", (req, response) => {
+  //   console.log(response.send(`hello world ${req.params.id}`));
+  
+    response.send(products);
+   
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
